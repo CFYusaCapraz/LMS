71efbfc138009284cd6e76e2eeea7424fedd2595
CyberFreak1911 <cyberfreak_1911@outlook.com>
2022-07-25 14:55:05 +0300

    Renamed packages and added Dashboard form.

---
+++ src/com/cyberfreak/lms/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cyberfreak.lms;
+
+import com.cyberfreak.form.Login;
+/**
+ *
+ * @author cyber
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Login login = new Login();
+        login.setVisible(true);
+    }
+
+}
